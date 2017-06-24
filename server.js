258c78e8be0b2fceb09fe33deb3525a5363e76bf
nickschrimpf <nicholas.schrimpf@mac.com>
2017-06-24 00:12:08 -0600

    added server and package.json changes

---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+const express = require('express');
+const app = express();
+const path = require('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+// Start the app by listening on the default
+// Heroku port
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    next(err), {
+      message: err.message,
+      error: err
+    };
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  next(err), {
+    message: err.message,
+    error: {}
+  };
+});
+
+
+
+app.listen(process.env.PORT || 8080);
